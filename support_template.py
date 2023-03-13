ca635205adb129cef623d802da5bd9159f5e4dbc
PimpMyGit <tommasoredaelli276@gmail.com>
2023-03-13 22:01:33 +0100

    add support template

---
+++ support_template.py
@@ -0,0 +1,14 @@
+# !!! Watch ya steps - don't commit this file: 
+# rename this into '_support.py' as it will be ignored by .gitignore (and imported in igs_test.ipynb)
+
+login_account_info = {
+    'username':     '<your-ig-username>',
+    'password':     '<your-ig-password>'
+}
+
+ig_profiles = {
+    'profile1':     '<profile1-username>',
+    'profile2':     '<profile2-username>'
+    
+    # ... enojy :)
+}
